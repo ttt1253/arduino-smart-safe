95aa85101d543713619f70a76072d903529beb11
cotidie <daily142857@gmail.com>
2023-12-08 22:03:37 +0900

    Initial Commit

---
+++ main/main.ino
@@ -0,0 +1,21 @@
+int password[] = {0, 0, 0, 0};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // 1. 사용자의 비밀번호 확인
+  readPassword();
+
+  // 2. LCD에 표시
+  displayPassword();
+
+  // 3. 버튼이 눌리면 비밀번호가 맞는지 확인한다.
+  if (isCorrectPassword()) {
+    lockSafe();
+  } else {
+    unlockSafe();
+  }
+}
