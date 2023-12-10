24cde6cd2413519b0422f008bef4c41233c360c4
Lee-Sangho <tkdgh5379@gmail.com>
2023-12-10 16:04:37 +0900

    lcd 화면 출력

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,20 @@
+#include <LiquidCrystal.h>
+
 int password[] = {0, 0, 0, 0};
-int numWrongPassword = 0;
+int WrongCount = 0;
+int potenpin[] = {A0, A1, A2, A3}; // 가변저항 핀
+int set_password[] = {0, 0, 0, 0};
+LiquidCrystal lcd(44, 45, 46, 47, 48, 49); //LCD 연결
 
 void setup() {
   // put your setup code here, to run once:
+  for (int i = 0; i < 4; i++) {
+    pinMode(potenpin[i], INPUT);
+  }
+  lcd.begin(16,2);
 
 }
+
 
 void loop() {
   // 1. 사용자의 비밀번호 확인
@@ -18,5 +28,54 @@ void loop() {
     lockSafe();
   } else {
     unlockSafe();
+  }
+}
+
+
+void readPassword() {
+  for (int i = 0; i < 4; i++) {
+    int readpin = analogRead(potenpin[i]); // 가변저항 값 읽기
+    password[i] = map(readpin, 0, 1023, 0, 9); // 가변저항 값 대입
+  }
+}
+
+bool isCorrectPassword() {
+  for (int i = 0; i < 4; i++) {
+    if (set_password[i] != password[i]) {
+      return false;
+      WrongCount++;
+    }
+  }
+  return true;
+}
+
+void displayPassword(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Enter Password");
+  lcd.setCursor(0,1);
+  for(int i=0; i<4; i++){
+    lcd.print("[ ");
+    lcd.print(password[i]);
+    lcd.print(" ]");
+  }
+}
+
+void displayjudgePassword(){
+  lcd.clear();
+  if(isCorrectPassword()){  // 비밀번호 일치 시 화면 출력
+    lcd.setCursor(0,0);
+    lcd.print("Correct Password");
+    lcd.setCursor(0,1);
+    lcd.print("Open the door");
+    delay(2000);
+  }else{ // 비밀번호 틀릴 시 화면 출력
+    lcd.setCursor(0,0);
+    lcd.print("Wrong Password");
+    lcd.setCursor(0,1);
+    lcd.print("Wrong "); // 틀린 횟수 출력
+    lcd.print(WrongCount);
+    lcd.print(" time(s)");
+    delay(2000);
   }
 }
