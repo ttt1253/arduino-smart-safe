704cc15a8ad08962d74af0e97a0f3238bb7120fc
cotidie <daily142857@gmail.com>
2023-12-12 13:37:11 +0900

    docs: 주석 제거

--- main/main.ino
+++ main/main.ino
@@ -75,11 +75,8 @@ void run(Status status) {
     case OPEN:
       break;
     case CLOSED:
-      // 1. 사용자의 비밀번호 확인
       readPassword();
-      // 2. LCD에 표시
       displayPassword();
-      // 3. 버튼이 눌리면 비밀번호가 맞는지 확인한다. 오류메시지 추가
       if (isOKButtonPressed()){
         if (isCorrectPassword()) {
           unlockSafe();
