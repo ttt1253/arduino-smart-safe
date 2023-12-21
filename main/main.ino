e807ab400e655778c7e078dfc37fda791c2f88dc
kartee0 <129240719+kartee0@users.noreply.github.com>
2023-12-21 19:36:23 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -16,5 +16,32 @@ bool isSafeClosed() {
   distance = duration * 0.034 / 2;
   if (distance <= detect) {
     return true;
+  } else {
+    return false;
+}
+
+//이 밑의 코드는 5초 이상 닫힘 감지시 닫히는 코드
+int closeDuration = 5000; 
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  if (distance <= closeThreshold) { //cm 이하 감지
+    static unsigned long startTime = 0;
+    if (millis() - startTime >= closeDuration) {
+      // 문을 닫는 코드
+      startTime = 0;
+    } else {
+      if (startTime == 0) {
+        startTime = millis();
+      }
+    }
+  } else {
+    startTime = 0;
   }
+  delay(1000);
 }
