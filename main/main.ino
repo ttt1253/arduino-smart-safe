210a7bf291d5ff8d4f2ce18cb7d653ee6faed319
Lee-Sangho <tkdgh5379@gmail.com>
2023-12-23 14:52:55 +0900

    reset display

--- main/main.ino
+++ main/main.ino
@@ -168,7 +168,12 @@ void displayjudgePassword(){
 
 // TODO(상호): 비밀번호 초기화 LCD 화면
 void displayResetPassword() {
-
+  if(isResetButtonPressed()){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Reset Password");
+    delay(1000);
+  }
 }
 
 void resetPassword() {
