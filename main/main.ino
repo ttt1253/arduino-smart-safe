1edcc620e2c952cc36038dcec083106a5c8f3c9e
Lee-Sangho <tkdgh5379@gmail.com>
2023-12-23 15:11:20 +0900

    fix: 리셋버튼 체크 부분 삭제

--- main/main.ino
+++ main/main.ino
@@ -168,12 +168,10 @@ void displayjudgePassword(){
 
 // TODO(상호): 비밀번호 초기화 LCD 화면
 void displayResetPassword() {
-  if(isResetButtonPressed()){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Reset Password");
-    delay(1000);
-  }
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Reset Password");
+  delay(1000);
 }
 
 void resetPassword() {
